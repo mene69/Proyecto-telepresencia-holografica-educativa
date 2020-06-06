9cd447551f8f781d69ffb1666fab7574190ffd5b
Daniel Meneses <danielmeneses26@gmail.com>
2020-06-06 19:52:53 -0400

    Co-authored-by: Fmauro98 <9fmauro8@gmail.com>

--- menu-principal.php
+++ menu-principal.php
@@ -1,98 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>Menu Principal</title>
-  <meta content="" name="descriptison">
-  <meta content="" name="keywords">
-
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Anyar - v2.0.0
-  * Template URL: https://bootstrapmade.com/anyar-free-multipurpose-one-page-bootstrap-theme/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
-
-<body>
-
-  <!-- ======= Barra superior ======= -->
-  
-  <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top ">
-    <div class="container d-flex align-items-center">
-      <div class="contact-info mr-auto">
-        <ul>
-          <li><i class="icofont-envelope"></i><a href="mailto:contact@example.com">t.e.h.echile@gmail.com</a></li>
-          <li><i class="icofont-phone"></i> +569 58224930 </li>
-          <li><i class="icofont-clock-time icofont-flip-horizontal"></i> Lun-Vier 9am - 5pm</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top ">
-    <div class="container d-flex align-items-center">
-
-      <h1 class="logo mr-auto"><a href="#header" class="scrollto">TEHE</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="#header" class="logo mr-auto scrollto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-      <nav class="nav-menu d-none d-lg-block">
-        <ul>
-          <li class="active"><a href="#header">Menu Principal</a></li>
-          <li><a href="#contact">Contactos</a></li>
-          <li><a href="#contact">Realizar llamada</a></li>
-          <li><a href="#services">¿Ayuda?</a></li>
-          <li><a href="#portfolio">Modulo de plantillas</a></li>
-          <li><a href="#team">Bienvenido(a) "ROL" </a></li>
-          <li><a href="blog.html">Editar Perfil</a></li>
-          <li><a href="#">Cerrar Sesión</a>
-            <!--<ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="drop-down"><a href="#">Deep Drop Down</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>-->
-          </li>
-        </ul>
-      </nav><!-- .nav-menu -->
-
-    </div>
-  </header>
-  <!-- End Header -->
+<?php include ("assets/Templates/header.php");?>
+<?php include ("assets/Templates/menu-usuario.php");?>
 
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex justify-cntent-center align-items-center">
@@ -146,39 +54,47 @@
 
 
 
-    <!-- ======= About Us Section ======= -->
+    <!-- ======= Inición Sección sobre nosotros ======= -->
     <section id="about" class="about">
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
-          <h2>About Us</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <h2>Sobre Nosotros</h2>
+          <p>Somos un grupo de estudiantes de la universidad tecnológica de INACAP, que buscar implementar una nueva forma o herramienta en la educación tradicional, la cual busque ser un producto que beneficie a esta misma.</p>
         </div>
 
         <div class="row content">
           <div class="col-lg-6">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
+              El objetivo principal del proyecto es elaborar una plataforma educativa utilizando la tecnología de hologramas
+              para proveer una nueva herramienta educativa para el profesor y sus estudiantes.
             </p>
             <ul>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
-              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+              <p>La problemática a solucionar es la siguiente: </p>
+              <p> La Educación remota rutinaria y básica. </p>
+              <br>
+              <p>¿los efectos y consecuencias?</p>
+              <li><i class="ri-check-double-line"></i> No alcanzar el aprendizaje esperado por el alumno</li>
+              <li><i class="ri-check-double-line"></i> Desmotivación de los alumnos</li>
+              <li><i class="ri-check-double-line"></i> En chile en el año 2019 hubo una tasa de retención de 85,8%</li>
             </ul>
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0">
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
+              <ul>
+                <br>
+                <p>Causas y Origen del problema</p>
+                <li><i class="ri-check-double-line"></i> Falta de interaccion entre alumno y profesor</li>
+                <li><i class="ri-check-double-line"></i> Falta de participación, provocada por la rutina diaria</li>
+                <li><i class="ri-check-double-line"></i> Las faltas de medios económicos impide obtener 
+                                                          servicios de educación online</li>
+              </ul>
             </p>
-            <a href="#" class="btn-learn-more">Learn More</a>
           </div>
         </div>
 
       </div>
-    </section><!-- End About Us Section -->
+    </section><!-- Fin Sección sobre nosotros -->
 
     <!-- ======= Why Us Section ======= -->
     <section id="why-us" class="why-us">
@@ -186,44 +102,44 @@
 
         <div class="row">
 
-          <div class="col-lg-5 align-items-stretch video-box" style='background-image: url("assets/img/why-us.jpg");' data-aos="fade-right">
-            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+          <div class="col-lg-5 align-items-stretch video-box" style='background-image: url("assets/img/Holograma_2020-Presentacion.png");' data-aos="fade-right">
+            <a href="https://www.youtube.com/watch?v=4c5aXKdoy8g&t=69s" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
           </div>
 
           <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch" data-aos="fade-left">
 
             <div class="content">
-              <h3>Eum ipsam laborum deleniti <strong>velit pariatur architecto aut nihil</strong></h3>
+              <h3>Producto Principal: <strong>Caja Holgráfica Educativa</strong></h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                A continuación se resolverán las dudas de la construcción y más puntos importantes de la maqueta de la Caja Holográfica Educativa.
               </p>
             </div>
 
             <div class="accordion-list">
               <ul>
                 <li data-aos="fade-up" data-aos-delay="100">
-                  <a data-toggle="collapse" class="collapse" href="#accordion-list-1"><span>01</span> Non consectetur a erat nam at lectus urna duis? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <a data-toggle="collapse" class="collapse" href="#accordion-list-1"><span>01</span> ¿Como se hizo esta caja holográfica? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
                   <div id="accordion-list-1" class="collapse show" data-parent=".accordion-list">
                     <p>
-                      Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
+                      La Caja Holográfica se construyó a través de una impresora 3D la cual nos permite moldear correctamente el diseño de nuestro producto estrella.
                     </p>
                   </div>
                 </li>
 
                 <li data-aos="fade-up" data-aos-delay="200">
-                  <a data-toggle="collapse" href="#accordion-list-2" class="collapsed"><span>02</span> Feugiat scelerisque varius morbi enim nunc? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <a data-toggle="collapse" href="#accordion-list-2" class="collapsed"><span>02</span> ¿Que medidas tiene la caja holográfica educativa? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
                   <div id="accordion-list-2" class="collapse" data-parent=".accordion-list">
                     <p>
-                      Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+                      Las medidas de la Caja Holográfica Educativa son .
                     </p>
                   </div>
                 </li>
 
                 <li data-aos="fade-up" data-aos-delay="300">
-                  <a data-toggle="collapse" href="#accordion-list-3" class="collapsed"><span>03</span> Dolor sit amet consectetur adipiscing elit? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                  <a data-toggle="collapse" href="#accordion-list-3" class="collapsed"><span>03</span> ¿Que beneficios tiene la caja en la utilización de los holgramas? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
                   <div id="accordion-list-3" class="collapse" data-parent=".accordion-list">
                     <p>
-                      Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
+                      El beneficio que tiene la caja educativa en los hologramas es la visualización de imagenes en 3D, directamente en vivo y sincronizada en la plataforma web y movil.
                     </p>
                   </div>
                 </li>
@@ -319,7 +235,7 @@
               <i class="bx bx-help-circle icon-help"></i> <a data-toggle="collapse" href="#faq-list-3" class="collapsed"> ¿Cuánto dinero necesito para tener este producto? <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
               <div id="faq-list-3" class="collapse" data-parent=".faq-list">
                 <p>
-                  Actual mente nuestro producto no tiene fines de lucro, pero sí los planes para ganar ingresos es poder asociarnos a diversasintituciones para ofrecer nuestro servicios.
+                  Actual mente nuestro producto no tiene fines de lucro, pero sí los planes para ganar ingresos es poder asociarnos a diversas instituciones para ofrecer nuestro servicios.
                 </p>
               </div>
             </li>
@@ -337,7 +253,7 @@
               <i class="bx bx-help-circle icon-help"></i> <a data-toggle="collapse" href="#faq-list-5" class="collapsed">¿Cómo puedo ponerme en contacto con ustedes?<i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
               <div id="faq-list-5" class="collapse" data-parent=".faq-list">
                 <p>
-                  Te puedes pornerte en contacto con nosotros a traves de nuestro correo tehe@gmail.com para realizar diversas preguntas o buscar la utilización de nuestro servicio.
+                  Te puedes poner en contacto con nosotros a traves de nuestro correo T.E.H.E@gmail.com para realizar diversas preguntas o buscar la utilización de nuestro servicio.
                 </p>
               </div>
             </li>
@@ -350,82 +266,4 @@
   </main>
   <!-- End #main -->
 
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Links de cada modulo</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Menu Principal</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Contactos</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Realizar llamada</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">¿Ayuda?</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Modulo de plantillas</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h4>Contacto</h4>
-            <p>
-              Universidad Tecnologica · Avda. Fco de Aguirre 389<br>
-              La Serena, Coquimbo 1700000<br>
-              Chile <br><br>
-              <strong>Numero de Telefono:</strong> +569 58224930<br>
-              <strong>Correo:</strong> tehechile@gmail.com<br>
-            </p>
-
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-info">
-            <h3>Acerca de TEHE</h3>
-            <p>Links de redes sociales:</p>
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <!--<a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>-->
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>TEHE</span></strong> Todos los derechos reservados.
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/anyar-free-multipurpose-one-page-bootstrap-theme/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-  </footer>
-  
-  <!-- End Footer -->
-
-  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
-  <div id="preloader"></div>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script src="assets/vendor/venobox/venobox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
-</body>
-
-</html>
+  <?php include ("assets/Templates/footer.php");?>
